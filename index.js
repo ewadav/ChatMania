66faa9f235762d547ed1cfc18d6a9d28aa25676b
David Ewald <ewadav@uw.edu>
2013-07-18 20:44:34 -0700

    Backend Sockets mostly in place

--- index.js
+++ index.js
@@ -6,9 +6,23 @@ var express = require("express");
 var app = express();
 var port = 3700;
 
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);cd
+
 app.get("/",  function(req, res){
-	res.send("It works!");
+	res.render("page");
 })
 
-app.listen(port);
+app.use(express.static(__dirname + '/public'));
+
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function(socket)	{
+	sockets.emit('message', {message: 'Welcome to Chat Mania'});
+	sockets.on('send', function(data)	{
+		io.sockets.emit('message', data);
+	});
+});
+
 console.log("Listening on port " + port);
