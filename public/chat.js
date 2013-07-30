c584e987ae8ce91aee2011661965e05a783dc285
David Ewald <ewadav@uw.edu>
2013-07-29 19:27:03 -0700

    Added some styles, but need Stylus for them to work

--- public/chat.js
+++ public/chat.js
@@ -10,6 +10,7 @@ window.onload = function() {
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
     var name = document.getElementById("name");
+    var currentDate = new Date();
  
     socket.on('message', function (data) {
         if(data.message) {
@@ -18,7 +19,9 @@ window.onload = function() {
             for(var i=0; i<messages.length; i++) {
                 html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
                 html += messages[i].message + '<br />';
-                html += messages[i].time;
+                if(messages[i].time !=undefined)    {
+                    html += '<small>' + messages[i].time + '</small>' + '<br />';
+                }
             }
             content.innerHTML = html;
             content.scrollTop = content.scrollHeight;
@@ -32,7 +35,14 @@ window.onload = function() {
             alert("Please type your name!");
         } else {
             var text = field.value;
-            socket.emit('send', { message: text, username: name.value, time: Math.round(+new Date()/1000)});
+            var datetime = "Last Sync: " + currentDate.getDate() + "/"
+                + (currentDate.getMonth()+1)  + "/" 
+                + currentDate.getFullYear() + " @ "  
+                + currentDate.getHours() + ":"  
+                + currentDate.getMinutes() + ":" 
+                + currentDate.getSeconds();
+
+            socket.emit('send', { message: text, username: name.value, time: datetime});
             field.value = "";
         }
     };
