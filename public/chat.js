8ceb0f3554f6f8926e689da72db4f39de2769508
David Ewald <ewadav@uw.edu>
2013-09-09 15:43:20 -0700

    Added chatroom backend, also added socket usernames and rooms as well as disconnects and userlists

--- public/chat.js
+++ public/chat.js
@@ -3,40 +3,54 @@
 // The small real time chat application
 
 window.onload = function() {
- 
+    var userName;
+    
+    
     var socket = io.connect('http://localhost:3700');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
     var name = document.getElementById("name");
- 
-    socket.on('message', function (data) {
+    var userList = document.getElementById('userList');
+     
+    userName = prompt("Please Enter Your Username", "");
+    socket.emit('setUsername', userName);
+    
+    socket.on('message', function(data) {
         if(data.message) {
             $('#content').append(data.username + ": " + data.message + " "  + data.time + "<br>");
         } else {
             console.log("There is a problem:", data);
         }
     });
+    
+    socket.on('updateUserlist', function(sockets) {
+        userList.empty();
+        sockets.forEach(function(socket) {
+             userList.append(socket.username);
+         });
+    });
+    
+    socket.on('roomDisconnect', function(username) {
+        $(#content).append(username + " has disconnected!");
+    });
+ 
+    
  
     sendButton.onclick = function() {
-        if (name.value == "") {
-            alert("Please type your name!");
-        } else if (field.value == "") {
-            field.setAttribute('placeholder', 'Please enter a message');
-        } else {
-            var text = field.value;
-            var currentDate = new Date();
-            var datetime = "Sent: " + (currentDate.getMonth()+1) + "/"
+         socket.emit('send', { message: field.value, username: userName, time: getCurrentTimeString()});
+         field.value = "";
+         field.focus();
+    };
+    
+    function getCurrentTimeString() {
+        var datetimeString = "Sent: " + (currentDate.getMonth()+1) + "/"
                 + currentDate.getDate()  + "/" 
                 + currentDate.getFullYear() + " @ "  
                 + currentDate.getHours() + ":"  
                 + currentDate.getMinutes() + ":" 
-                + currentDate.getSeconds();
-
-            socket.emit('send', { message: text, username: name.value, time: datetime});
-            field.value = "";
-            field.focus();
-        }
-    };
+                + currentDate.getSeconds(); 
+        return datetimeString;
+    }
  
 }
