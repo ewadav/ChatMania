10ecaa1fdf9fa84ef60272d7797be86a38799652
David Ewald <ewadav@uw.edu>
2013-09-15 16:26:07 -0700

    Edited the chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,37 +1,57 @@
 // David F Ewald
 // ChatMania
 // The small real time chat application
+//http://psitsmike.com/2011/10/node-js-and-socket-io-multiroom-chat-tutorial/
+    
 
-    //http://psitsmike.com/2011/10/node-js-and-socket-io-multiroom-chat-tutorial/
-    var socket = io.connect('http://localhost:3700');
-    
-     
-    
-    
-    socket.on('message', function(message, username, timeString) {
-        if(message) {
-            $('#content').append("<b>" + username + ": </b>" + message + " "  + timeString + "<br>");
-        } else {
-            console.log("There is a problem:", data);
-        }
+    var socket = io.connect('http://localhost:8080');
+
+    // on connection to server, ask for user's name with an anonymous callback
+    socket.on('connect', function(){
+        // call the server-side function 'adduser' and send one parameter (value of prompt)
+        socket.emit('adduser', prompt("What's your name?"));
     });
-    
-    socket.on('updateUserlist', function(sockets) {
-        userList.empty();
-        sockets.forEach(function(socket) {
-             userList.append(socket.username);
-         });
+
+    // listener, whenever the server emits 'updatechat', this updates the chat body
+    socket.on('updatechat', function (username, data) {
+        $('#conversation').append('<b>'+username + ':</b> ' + data + '<br>');
     });
-    
- 
- window.onload = function() {   
- 
-    sendButton.onclick = function() {
-         socket.emit('send', { message: field.value, username: userName, time: getCurrentTimeString()});
-         field.value = "";
-         field.focus();
-    };
-}
+
+    // listener, whenever the server emits 'updaterooms', this updates the room the client is in
+    socket.on('updaterooms', function(rooms, current_room) {
+        $('#rooms').empty();
+        $.each(rooms, function(key, value) {
+            if(value == current_room){
+                $('#rooms').append('<div>' + value + '</div>');
+            }
+            else {
+                $('#rooms').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
+            }
+        });
+    });
+
+    function switchRoom(room){
+        socket.emit('switchRoom', room);
+    }
+
+    // on load of page
+    $(function(){
+        // when the client clicks SEND
+        $('#datasend').click( function() {
+            var message = $('#data').val();
+            $('#data').val('');
+            // tell server to execute 'sendchat' and send along one parameter
+            socket.emit('sendchat', message);
+        });
+
+        // when the client hits ENTER on their keyboard
+        $('#data').keypress(function(e) {
+            if(e.which == 13) {
+                $(this).blur();
+                $('#datasend').focus().click();
+            }
+        });
+    });
 
  function getCurrentTimeString() {
         var datetimeString = "Sent: " + (currentDate.getMonth()+1) + "/"
